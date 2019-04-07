0b2a11e7ffc299ae67ab8a2cd8c9f46db350cfcd
Tycho Grouwstra <tychogrouwstra@gmail.com>
2019-04-07 21:10:56 +0200

    add traces in tooltips

--- src/graph.py
+++ src/graph.py
@@ -14,12 +14,20 @@ def gen_dot(states, edges):
     )
 
     A.add_edges_from(edges)
+
     for node, entity_state in states.items():
         label = pretty_print(entity_state)
+        tooltip = intra_state_trace(entity_state)
         if A.has_node(node):
             A.get_node(node).attr['label'] = label
+            A.get_node(node).attr['tooltip'] = tooltip
         else:
-            A.add_node(node, label=label)
+            A.add_node(node, label=label, tooltip=tooltip)
+
+    for a, b in edges:
+        e = A.get_edge(a, b)
+        tooltip = inter_state_trace(states[a], states[b])
+        e.attr['tooltip'] = tooltip
 
     A.node_attr['shape']='circle'
     return A
