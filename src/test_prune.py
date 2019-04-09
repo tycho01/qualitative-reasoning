991d590fc78d6d29ef509583b9a3feac52692ab8
Tycho Grouwstra <tychogrouwstra@gmail.com>
2019-04-09 16:24:09 +0200

    port over extremity check to next_derivatives

--- src/test_prune.py
+++ src/test_prune.py
@@ -2,16 +2,68 @@ from prune import *
 from mock import *
 
 def test_next_states():
-    assert len(next_states(entity_state))
+    print(next_states(entity_state))
+    assert next_states(entity_state) == [
+        EntityState(container, {
+            'volume': QuantityPair(Volume.ZERO, Direction.NEUTRAL),
+            'inflow': QuantityPair(Inflow.PLUS, Direction.POSITIVE),
+            'outflow': QuantityPair(Outflow.ZERO, Direction.NEUTRAL)
+        })
+    ]
 
 def test_next_magnitudes():
-    assert len(next_magnitudes(entity_state))
+    assert next_magnitudes(entity_state) == [{
+        'inflow': Inflow.PLUS,
+        'outflow': Outflow.ZERO,
+        'volume': Volume.ZERO,
+    }]
+
+def test_next_magnitudes_point_interval():
+    container_state_point = {
+        'volume': (Volume.ZERO, Direction.POSITIVE),
+        'inflow': (Inflow.PLUS, Direction.POSITIVE),
+        'outflow': (Outflow.ZERO, Direction.NEUTRAL),
+    }
+    entity_state_point = make_entity_state(container, container_state_point)
+    assert next_magnitudes(entity_state_point) == [
+        # points move before ranges
+        {
+            'volume': Volume.PLUS,
+            'inflow': Inflow.PLUS,
+            'outflow': Outflow.ZERO,
+        }
+    ]
 
 def test_zip_pair():
     assert zip_pair(({'volume': Volume.PLUS}, {'volume': Direction.POSITIVE})) == {'volume': QuantityPair(Volume.PLUS, Direction.POSITIVE)}
 
 def test_next_derivatives():
-    assert len(next_derivatives(entity_state))
+    assert next_derivatives(entity_state) == [{
+        'volume': Direction.NEUTRAL,
+        'inflow': Direction.POSITIVE,
+        'outflow': Direction.NEUTRAL,
+    }]
+
+def test_next_derivatives_clipping():
+    container_state_clip = {
+        'volume': (Volume.MAX, Direction.POSITIVE),
+        'inflow': (Inflow.ZERO, Direction.NEUTRAL),
+        'outflow': (Outflow.ZERO, Direction.NEUTRAL),
+    }
+    entity_state_clip = make_entity_state(container, container_state_clip)
+    assert next_derivatives(entity_state_clip) == [
+        {
+            'volume': Direction.NEUTRAL,
+            'inflow': Direction.NEUTRAL,
+            'outflow': Direction.NEUTRAL,
+        },
+        {
+            # volume direction forced to neutral by the extremity check
+            'volume': Direction.NEUTRAL,
+            'inflow': Direction.NEUTRAL,
+            'outflow': Direction.POSITIVE,
+        }
+    ]
 
 def test_move_derivative():
     assert move_derivative(Direction.POSITIVE, Direction.QUESTION) == set([Direction.POSITIVE, Direction.NEUTRAL])
@@ -21,33 +73,6 @@ def test_move_magnitude():
     space = entity_state.entity.quantities[k].quantitySpace
     assert move_magnitude(QuantityPair(Volume.ZERO, Direction.NEUTRAL), space) == Volume.ZERO
     assert move_magnitude(QuantityPair(Volume.ZERO, Direction.POSITIVE), space) == Volume.PLUS
-
-# def test_check_extremes_good():
-#     container_state_good = {
-#         'volume': (Volume.MAX, Direction.NEUTRAL),
-#         'inflow': (Inflow.ZERO, Direction.POSITIVE),
-#         'outflow': (Outflow.ZERO, Direction.NEUTRAL),
-#     }
-#     entity_state_good = make_entity_state(container, container_state_good)
-#     assert check_extremes(entity_state_good) == True
-
-# def test_check_extremes_bad():
-#     container_state_bad = {
-#         'volume': (Volume.MAX, Direction.POSITIVE),  # can't have direction positive
-#         'inflow': (Inflow.ZERO, Direction.NEUTRAL),
-#         'outflow': (Outflow.ZERO, Direction.NEUTRAL),
-#     }
-#     entity_state_bad = make_entity_state(container, container_state_bad)
-#     assert check_extremes(entity_state_bad) == False
-
-# def test_extreme_direction():
-#     assert extreme_direction(Volume.ZERO, Volume) == Direction.NEGATIVE
-#     assert extreme_direction(Volume.PLUS, Volume) == Direction.NEUTRAL
-#     assert extreme_direction(Volume.MAX, Volume) == Direction.POSITIVE
-
-def test_state_derivatives():
-    state = {'volume': QuantityPair(Volume.ZERO, Direction.POSITIVE)}
-    assert state_derivatives(state) == {'volume': Direction.POSITIVE}
 
 def test_relation_effects():
     assert relation_effects(
